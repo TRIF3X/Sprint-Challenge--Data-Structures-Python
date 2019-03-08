09387e3b042841139edcf4681f13ea668b3c9bbf
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-08 10:30:15 -0600

    binary search tree names done

--- names/names.py
+++ names/names.py
@@ -11,10 +11,48 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+class BinarySearchTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    if self.left and self.right and self.value is None:
+      self.value = value
+    elif value < self.value:
+      if self.left is None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    else:
+      if self.right is None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+
+
+  def contains(self, target):
+    if target < self.value:
+      if self.left is None:
+        return False
+      return self.left.contains(target)
+    elif target > self.value:
+      if self.right is None:
+        return False
+      return self.right.contains(target)
+    else:
+      duplicates.append(target)
+
+
+b = BinarySearchTree('')
+for x in names_1:
+    b.insert(x)
+
+for y in names_2:
+    b.contains(y)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
